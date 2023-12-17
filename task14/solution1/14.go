ae07f8043f96ef5daa5690f6413aa2a7bf192357
leenzstra <leenzstra@gmail.com>
2023-12-17 19:53:11 +0500

    task 14 add new solution

---
+++ task14/solution1/14.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Разработать программу, которая в рантайме способна определить тип переменной:
+// int, string, bool, channel из переменной типа interface{}.
+
+func main() {
+	str := "asd"
+	num := 5
+	ok := false
+	ch := make(chan int)
+
+	values := []interface{}{str, num, ok, ch}
+
+	// Используем type switch. Он берет интерфейс и пытаемся преобразовать тип
+	for _, v := range values {
+		switch v.(type) {
+		case string:
+			fmt.Println(v, "string")
+		case bool:
+			fmt.Println(v, "bool")
+		case int:
+			fmt.Println(v, "int")
+		case chan int:
+			fmt.Println(v, "chan int")
+		default:
+			fmt.Println(v, "other type")
+		}
+	}
+}
